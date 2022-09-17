6f69ceda0e604529c286c8fa7b68f026290fe4aa
Alexandre <alexandremiguel.dev@gmail.com>
2022-09-16 21:08:22 -0300

    feat: add inter font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,14 @@ export default function Document() {
 	return (
 		<Html lang='pt'>
 			<Head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
-					href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&display=swap'
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin='true'
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
 					rel='stylesheet'
 				/>
 			</Head>
