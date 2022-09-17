c3496c3a9ccedaae7ff2eba632ac2de470bff5f2
Alexandre <alexandremiguel.dev@gmail.com>
2022-09-16 21:13:10 -0300

    feat: add ads api route

---
+++ src/pages/api/ads/[[...params]]/index.ts
@@ -0,0 +1,56 @@
+import AdsDTO from 'dto/AdsDTO'
+import cors from 'middlewares/with-cors'
+import {
+	Body,
+	Catch,
+	createHandler,
+	Get,
+	HttpCode,
+	NotFoundException,
+	Param,
+	Post,
+	UseMiddleware
+} from 'next-api-decorators'
+import { convertHourStringToMinutes } from 'utils/convert-hour-string-to-minutes'
+import exceptionHandler from 'utils/exception-handler'
+
+@UseMiddleware(cors)
+class Ads {
+	@Get('/:id/discord')
+	@HttpCode(200)
+	@Catch(exceptionHandler)
+	public async getDiscordByAdsId(@Param('id') id: string) {
+		const discord = await prisma.ad.findUnique({
+			select: {
+				discord: true
+			},
+			where: {
+				id: id
+			}
+		})
+
+		if (!discord) {
+			throw new NotFoundException('discord não encontrado.')
+		}
+
+		return discord
+	}
+
+	@Post('/:gameId')
+	@HttpCode(201)
+	@Catch(exceptionHandler)
+	public async createAd(@Param('gameId') gameId: string, @Body() body: AdsDTO) {
+		const { hourEnd, hourStart } = body
+
+		return await prisma.ad.create({
+			data: {
+				...body,
+				gameId,
+				hourStart: convertHourStringToMinutes(hourStart),
+				hourEnd: convertHourStringToMinutes(hourEnd)
+			}
+		})
+	}
+}
+
+export default createHandler(Ads)
